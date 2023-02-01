0f35123654848b5761f1aeba1d63ba1c86d9e998
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-02-01 20:38:32 +0000

    add deezer ajax request

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
 console.log("hey");
+
+let artist = "eminem"
+
+let settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artist,
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "ca3e3b7c4dmshcf0d18644a9b128p15b157jsnca8487f0f2a9",
+		"X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+

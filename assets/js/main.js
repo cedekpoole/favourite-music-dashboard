222b53d9566a2da2f5cf9ada0bd8ec5f30941815
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-02-01 20:55:29 +0000

    format code and add comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,33 @@
 console.log("hey");
 
-let artist = "eminem"
+// Using jQuery, connect this to the value of the search input
+let artist = "michael jackson";
 
+// Ajax request for artist (Deezer API)
 let settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artist,
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "ca3e3b7c4dmshcf0d18644a9b128p15b157jsnca8487f0f2a9",
-		"X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
-	}
+  async: true,
+  crossDomain: true,
+  url: "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artist,
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "ca3e3b7c4dmshcf0d18644a9b128p15b157jsnca8487f0f2a9",
+    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+  },
 };
 
 $.ajax(settings).done(function (response) {
-	console.log(response);
-	var songArtist = response.data[0].artist.name 
-	var songName = response.data[0].title
-	var urlStart = "https://some-random-api.ml/lyrics?title="
-	var queryURL = urlStart + songArtist + " " + songName
-
-	$.ajax({
-		url: queryURL,
-		method: "GET"
-	}).then(function(response) {
-		console.log(response)
-	})
-
+  console.log(response);
+// Get query URL for the lyric API
+  var queryURL =
+    "https://some-random-api.ml/lyrics?title=" +
+    response.data[0].artist.name +
+    " " +
+    response.data[0].title;
+// Send ajax request for lyrics data 
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
 });
-
-
-
-
-
-

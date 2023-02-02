60e4eb04da90bace1841fa939789e9e1dd6b3092
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-02-02 17:36:47 +0000

    add variable for API key in ajax settings

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ console.log("hey");
 
 function showLyricData() {
   // Using jQuery, connect this to the value of the search input
-  let artist = prompt("Enter artist name");
+  let artist = prompt("Enter artist name and song title");
 
   // Ajax request for artist (Deezer API)
   let settings = {
@@ -11,15 +11,15 @@ function showLyricData() {
     url: "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artist,
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "ca3e3b7c4dmshcf0d18644a9b128p15b157jsnca8487f0f2a9",
+      "X-RapidAPI-Key": personalAPIKey,
       "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
     },
   };
-  // Need to prompt user for song name here (depends on which card they click)
+  
   $.ajax(settings).done(function (response) {
     console.log(response);
     // Get query URL for the lyric API
-    // Dependent on song name given, change the index of response.data
+
     var queryURL =
       "https://some-random-api.ml/lyrics?title=" +
       response.data[0].artist.name +
@@ -34,5 +34,6 @@ function showLyricData() {
     });
   });
 }
+showLyricData()
 
 // $("**SEARCH BUTTON**").on("click", showLyricData);

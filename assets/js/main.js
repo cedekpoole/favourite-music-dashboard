2881a414a9ac204f33badb1cfb8cdd38277099cf
Bethany Ryalls <bethany@motifdesignstudio.com>
2023-02-02 19:28:48 +0000

    setting variables for API values and taking input from search bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,71 @@
 console.log("hey");
 
 function showLyricData() {
-  // Using jQuery, connect this to the value of the search input
-  let artist = prompt("Enter artist name and song title");
+    // Using jQuery, connect this to the value of the search input
+    let artist = $('#search-input').val();
 
-  // Ajax request for artist (Deezer API)
-  let settings = {
-    async: true,
-    crossDomain: true,
-    url: "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artist,
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "ca3e3b7c4dmshcf0d18644a9b128p15b157jsnca8487f0f2a9",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-    },
-  };
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-    // Get query URL for the lyric API
+    // Ajax request for artist (Deezer API)
+    let settings = {
+        async: true,
+        crossDomain: true,
+        url: "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artist,
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key": "ca3e3b7c4dmshcf0d18644a9b128p15b157jsnca8487f0f2a9",
+            "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+        },
+    };
 
-    var queryURL =
-      "https://some-random-api.ml/lyrics?title=" +
-      response.data[0].artist.name +
-      " " +
-      response.data[0].title;
-    // Send ajax request for lyrics data
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response.lyrics);
+    $.ajax(settings).done(function (songResponse) {
+        // console.log(songResponse);
+        // Get query URL for the lyric API
+
+        var songResults = songResponse.data;
+        var songResultsArray = [];
+
+        
+        // var resultsHeading = $('<h2> Songs by ' + artist.toUpperCase() + '</h2>');
+        // resultsHeading.attr('class', 'px-2 mt-4');
+        // $('#main').append(resultsHeading);
+
+        $.each(songResults, function (index, value) {
+            if (index == 9) {
+                return false;
+            }
+            songsObj = {
+                songTitle: value.title,
+                songArtist: value.artist.name,
+                coverImage: value.album.cover,
+                songAlbum: value.album.title
+            }
+            console.log(songsObj);
+
+            songResultsArray.push(songsObj);
+        });
+
+        for (let i = 0; i < songResultsArray.length; i++) {
+            
+        };
+
+
+
+
+        // var queryURL =
+        //     "https://some-random-api.ml/lyrics?title=" +
+        //     songResponse.data[0].artist.name +
+        //     " " +
+        //     songResponse.data[0].title;
+        // // Send ajax request for lyrics data
+        // $.ajax({
+        //     url: queryURL,
+        //     method: "GET",
+        // }).then(function (response) {
+        //     console.log(response.lyrics);
+        // });
     });
-  });
 }
 showLyricData()
 
-// $("**SEARCH BUTTON**").on("click", showLyricData);
+$("#search-button").on("click", showLyricData);
+
+

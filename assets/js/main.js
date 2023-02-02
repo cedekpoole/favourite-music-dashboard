9f4618c14fb9c8ee5cd60fe681ecf8fa150ec52b
Bethany Ryalls <bethany@motifdesignstudio.com>
2023-02-02 20:41:44 +0000

    adding loop to add bootstrap cards for each result

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,21 @@
 console.log("hey");
 
-function showLyricData() {
+function showLyricData(e) {
+
+    e.preventDefault();
     // Using jQuery, connect this to the value of the search input
     let artist = $('#search-input').val();
+
+    //clearing search input field
+    $('#search-input').val('');
+
+    // adding heading for chosen artist
+    var resultsHeading = $('<h2> Songs by ' + artist + '</h2>');
+    resultsHeading.attr('class', 'px-2 mt-4');
+    resultsHeading.css('textTransform', 'capitalize');
+    $('#main').prepend(resultsHeading);
+
+
 
     // Ajax request for artist (Deezer API)
     let settings = {
@@ -16,22 +29,22 @@ function showLyricData() {
         },
     };
 
+
+
     $.ajax(settings).done(function (songResponse) {
         // console.log(songResponse);
         // Get query URL for the lyric API
 
+        // store song response information
         var songResults = songResponse.data;
         var songResultsArray = [];
-
         
-        // var resultsHeading = $('<h2> Songs by ' + artist.toUpperCase() + '</h2>');
-        // resultsHeading.attr('class', 'px-2 mt-4');
-        // $('#main').append(resultsHeading);
-
+        // loop through the data for the first 9 responses
         $.each(songResults, function (index, value) {
-            if (index == 9) {
+            if (index == 12) {
                 return false;
             }
+            // get the title, artist, cover image and album for each song
             songsObj = {
                 songTitle: value.title,
                 songArtist: value.artist.name,
@@ -43,9 +56,38 @@ function showLyricData() {
             songResultsArray.push(songsObj);
         });
 
+        currentIteration = 0;
+
+        // loop through the stored song info and display in bootstrap cards
         for (let i = 0; i < songResultsArray.length; i++) {
-            
+            if (currentIteration % 4 === 0) {
+                row = $('<div class="row w-100 justify-content-between"></div>');
+                $('#cardContainer').append(row);
+            }
+            //create div to hold each card
+            var songResultContainer = $('<div>');
+            songResultContainer.attr('class', 'col-lg-2 col-sm-6 justify-content-between');
+            row.append(songResultContainer);
+
+            //create bootstrap card
+            var songResultCard = $('<div>');
+            songResultCard.attr('class', 'card result mt-4');
+            // songResultCard.attr('style', 'width: 18rem;');
+            songResultContainer.append(songResultCard);
+
+            //add image to card
+            var songResultImg = $('<img>');
+            songResultImg.attr('class', 'card-img-top');
+            songResultImg.attr('src', songResultsArray[i].coverImage);
+            songResultCard.append(songResultImg);
+
+            // var songResultBody = 
+
+            currentIteration++;
+
         };
+
+        $('#cardContainer').append(row);
 
 
 
@@ -63,8 +105,7 @@ function showLyricData() {
         //     console.log(response.lyrics);
         // });
     });
-}
-showLyricData()
+};
 
 $("#search-button").on("click", showLyricData);
 
